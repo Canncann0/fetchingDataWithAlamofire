5696303d4dda8291bf2c8ae20815ceef70ca4900
Dogancan <turgutdogancan0@gmail.com>
2025-11-19 20:15:43 +0300

    Initial Commit

---
+++ fetchingDataWithAlamofire/fetchingDataWithAlamofireApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetchingDataWithAlamofireApp.swift
+//  fetchingDataWithAlamofire
+//
+//  Created by Dogancan turgut on 19.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct fetchingDataWithAlamofireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
